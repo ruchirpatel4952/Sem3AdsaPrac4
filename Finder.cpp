1c028dc038651159732b018a0af4aa84512c4919
Ruchir <ruchirpatel2000@gmail.com>
2024-04-01 00:36:23 +1030

    first commit

--- Finder.cpp
+++ Finder.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Finder {
 public:
-    vector<int> findSubstrings(string s1, string s2) {
+    std::vector<int> Finder::findSubstrings(const std::string& s1, const std::string& s2) {
         vector<int> result(s2.size(), -1); // Initialize results with -1
         for (size_t i = 0; i < s1.size(); ++i) {
             for (size_t j = 0; j < s2.size() && i + j < s1.size(); ++j) {
