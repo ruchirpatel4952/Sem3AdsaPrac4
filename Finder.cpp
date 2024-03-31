eb057218ca102e6a06315905d0f8c7180622cc68
Ruchir <ruchirpatel2000@gmail.com>
2024-03-31 23:30:08 +1030

    first commit

--- Finder.cpp
+++ Finder.cpp
@@ -1,16 +1,20 @@
 #include "Finder.h"
+
 using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
-    bool prefixNotFound  = false;
-
-    for(size_t i = 1; i <= s2.size() && !prefixNotFound; i++) {
-        size_t found = s1.find(s2.substr(0, i));
-        if (found != string::npos) {
-            result.push_back(found);
+    size_t lastFound = 0; 
+    bool foundAllPrevious = true; 
+    for (size_t i = 1; i <= s2.size() && foundAllPrevious; ++i) {
+        string prefix = s2.substr(0, i);
+        size_t foundPos = s1.find(prefix, lastFound);
+        
+        if (foundPos != string::npos) {
+            result.push_back(foundPos);
+            lastFound = foundPos + 1;
         } else {
-            prefixNotFound = true; 
+            foundAllPrevious = false; 
             result.push_back(-1);
         }
     }
