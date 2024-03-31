4b46f6e96cef626f3dff7ce6c873e56d1d927669
Ruchir <ruchirpatel2000@gmail.com>
2024-04-01 00:47:10 +1030

    first commit

--- Finder.cpp
+++ Finder.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 vector<int> Finder::findSubstrings(const std::string& s1, const std::string& s2) {
-    vector<int> result(s2.size(), -1); // Initialize results with -1
+    vector<int> result(s2.size(), -1); 
     for (size_t i = 0; i < s1.size(); ++i) {
         for (size_t j = 0; j < s2.size() && i + j < s1.size(); ++j) {
-            if (s1[i + j] != s2[j]) break; // If any character doesn't match, stop this prefix
-            if (result[j] == -1) result[j] = i; // Record the first match position for each prefix
+            if (s1[i + j] != s2[j]) break; 
+            if (result[j] == -1) result[j] = i; 
         }
     }
     return result;
