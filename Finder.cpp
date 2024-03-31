aec8fe78b2b202d40ede3b40fbe0604bddf18bb6
Ruchir <ruchirpatel2000@gmail.com>
2024-03-31 23:41:26 +1030

    first commit

--- Finder.cpp
+++ Finder.cpp
@@ -1,26 +1,17 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include "Finder.h"
 
-class Finder {
-public:
-    std::vector<int> findSubstrings(const std::string& s1, const std::string& s2) {
-        std::vector<int> result;
+std::vector<int> Finder::findSubstrings(const std::string& s1, const std::string& s2) {
+    std::vector<int> result;
 
-        for (size_t i = 1; i <= s2.size(); ++i) {
-            // Generate the current prefix from s2
-            std::string prefix = s2.substr(0, i);
-            // Search for this prefix in s1
-            size_t found = s1.find(prefix);
-            if (found != std::string::npos) {
-                // If found, store the index
-                result.push_back(found);
-            } else {
-                // If not found, store -1
-                result.push_back(-1);
-            }
+    for (size_t i = 1; i <= s2.length(); ++i) {
+        std::string prefix = s2.substr(0, i);
+        size_t found = s1.find(prefix);
+        if (found != std::string::npos) {
+            result.push_back(found);
+        } else {
+            result.push_back(-1);
         }
-
-        return result;
     }
-};
+
+    return result;
+}
